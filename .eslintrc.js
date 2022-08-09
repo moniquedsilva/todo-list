a62efea7ac2b88a73d847d17656ff1874fe1a02f
Monique Silva <moniquesntsilva@gmail.com>
2022-08-08 21:11:39 -0300

    feat: add eslint standard and plugins

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  extends: ['plugin:vue/vue3-essential', '@vue/standard', 'plugin:vuejs-accessibility/recommended'],
+
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+
+  rules: {
+    quotes: [2, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/no-named-as-default': 0,
+    'vue/no-v-html': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }],
+    'space-before-function-paren': [
+      2,
+      { anonymous: 'always', named: 'never', asyncArrow: 'always' }
+    ]
+  }
+}
