5e2fa1def5b60d151c6a03a2237131a615d0cff6
Monique Silva <moniquesntsilva@gmail.com>
2022-08-12 00:14:57 -0300

    feat: bind class and add function to remove and update

--- src/components/List/List.vue
+++ src/components/List/List.vue
@@ -8,13 +8,26 @@
     >
       <button
         aria-label="check"
-        class="h-5 w-5 justify-self-start rounded-full border-3 border-solid border-gray-300 transition duration-300 ease-in hover:border-primary"
+        :aria-checked="task.completed"
+        @click="toggleCompleted(task)"
+        :class="[
+          'h-5 w-5 justify-self-start rounded-full border-3 border-solid border-gray-400 transition duration-300 ease-in hover:border-gray-400',
+          { 'border-gray-400 bg-gray-400': task.completed }
+        ]"
       ></button>
-      <p class="col-span-6 cursor-pointer justify-self-start font-semibold">
+      <p
+        @click="toggleCompleted(task)"
+        @keydown.enter="toggleCompleted(task)"
+        :class="[
+          'translate col-span-6 cursor-pointer justify-self-start font-semibold duration-300 ease-in',
+          { 'text-gray-400 line-through decoration-2': task.completed }
+        ]"
+      >
         {{ task.title }}
       </p>
       <button
         class="h-4 w-4 justify-self-end opacity-30 transition-opacity duration-200 ease-in hover:opacity-100"
+        @click="removeTask(index)"
       >
         <img src="/assets/x.svg" alt="x" />
       </button>
@@ -34,6 +47,14 @@ export default {
   computed: {
     ...mapState(['tasks']),
     ...mapGetters(['finishedTasksLength'])
+  },
+  methods: {
+    removeTask(index) {
+      this.$store.dispatch('REMOVE', index)
+    },
+    toggleCompleted(index) {
+      this.$store.dispatch('UPDATE', index)
+    }
   }
 }
 </script>
